706c74300c26eae0518e3da2d45b2db8c90179c6
joshgressman <joshgressman@gmail.com>
2017-05-23 15:05:44 -0500

    added app wide service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,5 +1,9 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
 
 
+
+@Injectable()
 export class AccountsService {
 
   accounts = [
@@ -17,12 +21,16 @@ export class AccountsService {
     }
   ];
 
+  constructor( private loggingService: LoggingService){}
+
   addAccount(name: string, status: string){
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, statis: string){
    this.accounts[id].status = status;
+   this.loggingService.logStatusChange(status);
   }
 
 }
